61dc90065ba2a0838923c5f1b0b385835e0a6c4f
Stephen Roille <stephen.roille@gmail.com>
2021-09-22 14:28:59 +0200

    14:28:59

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM alpine
-RUN apk add --no-cache bash httpie jq
+RUN apk update && apk add --no-cache bash httpie jq py3-pip
 COPY entrypoint.sh /entrypoint.sh
+# COPY event.json /event.json
 RUN chmod +x /entrypoint.sh
+# ENV GITHUB_EVENT_PATH=/event.json
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 # exit immediately on error
 set -e
-if echo "$*" | grep -i -q fix;
+
+if [ -z "$GITHUB_EVENT_PATH" ];
 then
-    echo This is a fix
+    echo environment variable "GITHUB_EVENT_PATH" required
+    exit 1
 else
-    echo nothing to process
+    echo success: "$GITHUB_EVENT_PATH"
+fi
+
+if jq '.commits[].message, .head_commit.message' < "$GITHUB_EVENT_PATH" | grep -i "$@";
+then
+    echo "found keyword '$@' in commit message"
+    VERSION=$(date +%F.%s)
+
+    DATA="$(printf '{"tag-name":"v%s", ' $VERSION)"
+    DATA="$DATA $(printf '"target_commitish":"master",')"
+    DATA="$DATA $(printf '"name":"v%s",' $VERSION)"
+    DATA="$DATA $(printf '"body":"Automated release based on keyword: %s",' "$@")"
+    DATA="$DATA $(printf '"draft":false, "prerelease":false}')"
+    URL="https://api.github.com/repos/$GITHUB_REPOSITORY/releases?access_token=$GITHUB_TOKEN"
+    echo $DATA
+    echo $URL
+    echo $DATA | http POST $URL | jq .
+else
+    echo "nothing to process"
 fi
